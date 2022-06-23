f39759c82312baa3de1fa43f1ad06e2ba849d3ae
lien-jiseo <jiin@lien.kr>
2022-06-23 15:36:56 +0900

    Board의 handleClick을 Game으로 이동
    - handleClick 이동 후 수정

--- src/index.jsx
+++ src/index.jsx
@@ -14,20 +14,11 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ squares, xIsNext: !this.state.xIsNext });
-  }
-
   renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
-        onClick={() => this.props.Click(i)}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -74,6 +65,24 @@ class Game extends React.Component {
       ],
       xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const { history } = this.state;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
