ee17a5bed84379dc2d7743e37d6a326819c759d5
lien-jiseo <jiin@lien.kr>
2022-06-23 15:29:20 +0900

    Game render() 변경
    - history를 사용하도록 업데이트
    - 게임의 상태 확인하고 표시

--- src/index.jsx
+++ src/index.jsx
@@ -27,7 +27,7 @@ class Board extends React.Component {
     return (
       <Square
         value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => this.props.Click(i)}
       />
     );
   }
@@ -78,13 +78,25 @@ class Game extends React.Component {
   }
 
   render() {
+    const { history } = this.state;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + 'winner';
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
