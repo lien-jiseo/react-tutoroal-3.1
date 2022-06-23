c5dd0f8e85204cf8b64368122ae8ee14aa115a65
lien-jiseo <jiin@lien.kr>
2022-06-23 11:44:48 +0900

    handleClcik에 xIsNext 값 뒤집기
    - squares[i] = this.state.xIsNext ? 'X' : 'O 로 변경
    - this.setState({ squares, xIsNext: !this.state.xIsNext }) 추가

--- src/index.jsx
+++ src/index.jsx
@@ -24,8 +24,8 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {
