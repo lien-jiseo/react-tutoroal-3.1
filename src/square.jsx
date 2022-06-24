381a89e80c47cbc11765cce4574c0423463d4ca0
lien-jiseo <jiin@lien.kr>
2022-06-24 11:34:29 +0900

    function Square 수정
    - value 수정

--- src/square.jsx
+++ src/square.jsx
@@ -1,16 +1,15 @@
-/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
 export default function Square(props) {
-  const { onClick } = props;
+  const { onClick, value } = props;
   return (
     <button
       type="button"
       className="square"
       onClick={() => onClick()}
     >
-      {props.value}
+      {value}
     </button>
   );
 }
