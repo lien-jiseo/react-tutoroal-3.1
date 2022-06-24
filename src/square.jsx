c20986ff9a73e41663882c68c8881a1ac1472377
lien-jiseo <jiin@lien.kr>
2022-06-24 11:22:27 +0900

    function-square-수정
    - onClick 수정
    - eslint 오류 수정

--- src/square.jsx
+++ src/square.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 export default function Square(props) {
+  const { onClick } = props;
   return (
     <button
       type="button"
       className="square"
-      onClick={props.onClick}
+      onClick={() => onClick()}
     >
       {props.value}
     </button>
